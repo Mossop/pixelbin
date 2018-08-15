010c33db0e81b71c08d720708cf360d1d4051167
Dave Townsend <dtownsend@oxymoronical.com>
2018-08-14 21:34:59 -0700

    Allow logging in.

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signin', views.signin),
+    path('login', views.login),
     path('', views.index),
     path('upload', views.upload),
-    path('login', views.login),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,35 @@
 from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.template import loader
 
+def signin(request):
+    next = request.GET['next']
+    template = loader.get_template('app/signin.html')
+    context = {
+        'next': next,
+        'target': reverse(login)
+    }
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    next = request.POST['next']
+
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return redirect(next)
+
+    return HttpResponse("Bad Login.")
+
+@login_required(login_url='/app/signin')
 def index(request):
     return HttpResponse("Index.")
 
-def login(request):
-    return HttpResponse("Login.")
-
+@login_required(login_url='/app/signin')
 def upload(request):
     return HttpResponse("Upload.")
