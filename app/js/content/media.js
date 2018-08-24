f57613d8d27eb7a386737795ac932c225f8aad49
Dave Townsend <dtownsend@oxymoronical.com>
2018-08-24 16:23:58 -0700

    Fetch the thumbnail as a blob and then render it to a canvas.

--- app/js/content/media.js
+++ app/js/content/media.js
@@ -3,19 +3,51 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { buildThumbURL } from "../api/media";
+import { drawBitmapToCanvas } from "../utils/helpers";
 
 const mapStateToProps = () => ({
   thumbsize: 150,
 });
 
-const Media = ({ thumbsize, media }) => {
-  let thumburl = buildThumbURL(media.id, thumbsize);
-  return (
-    <div className="media">
-      <div style={{ width: thumbsize, height: thumbsize, background: `url(${thumburl}) center center no-repeat` }}></div>
-    </div>
-  );
-};
+class Media extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.state = {
+      bitmap: null,
+    };
+  }
+
+  async componentDidMount() {
+    let thumburl = buildThumbURL(this.props.media.id, this.props.thumbsize);
+
+    let response = await fetch(thumburl);
+    if (response.ok) {
+      let blob = await response.blob();
+      let bitmap = await createImageBitmap(blob);
+      this.setState({
+        bitmap,
+      });
+    }
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.bitmap || !this.state.bitmap) {
+      return;
+    }
+
+    drawBitmapToCanvas(this.state.bitmap, this.canvasRef.current);
+  }
+
+  render() {
+    let { thumbsize } = this.props;
+    return (
+      <div className="media">
+        <canvas ref={this.canvasRef} height={thumbsize} width={thumbsize} style={{ width: thumbsize, height: thumbsize }}/>
+      </div>
+    );
+  }
+}
 
 Media.propTypes = {
   thumbsize: PropTypes.number.isRequired,
--- app/js/content/upload.js
+++ app/js/content/upload.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import { drawBitmapToCanvas } from "../utils/helpers";
+
 const mapStateToProps = () => ({
   thumbsize: 150,
 });
@@ -12,24 +14,8 @@ class Upload extends React.Component {
     this.canvasRef = React.createRef();
   }
 
-  drawThumbnail() {
-    let size = this.props.thumbsize;
-    let width = this.props.bitmap.width;
-    let height = this.props.bitmap.height;
-    if (width > height) {
-      width = size;
-      height = this.props.bitmap.height / (this.props.bitmap.width / width);
-    } else {
-      height = size;
-      width = this.props.bitmap.width / (this.props.bitmap.height / height);
-    }
-
-    let context = this.canvasRef.current.getContext("2d");
-    context.drawImage(this.props.bitmap, (size - width) / 2, (size - height) / 2, width, height);
-  }
-
   componentDidMount() {
-    this.drawThumbnail();
+    drawBitmapToCanvas(this.props.bitmap, this.canvasRef.current);
   }
 
   render() {
