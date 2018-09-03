0885015d50820e0c7273745b773fa3f2f5c76593
Dave Townsend <dtownsend@oxymoronical.com>
2018-09-02 21:35:35 -0700

    Allow filtering by media type.

--- app/js/content/MediaList.js
+++ app/js/content/MediaList.js
@@ -3,6 +3,7 @@ import { PropTypes } from "prop-types";
 
 import Media from "./Media";
 import { bindAll } from "../utils/helpers";
+import { If, Then, Else } from "../utils/if";
 
 function sort(arr, cmp) {
   arr = arr.slice(0);
@@ -28,6 +29,12 @@ class MediaList extends React.Component {
     });
   }
 
+  selectType(type) {
+    this.setState({
+      type,
+    });
+  }
+
   render() {
     let media = this.props.media;
     media = sort(media, (a, b) => {
@@ -38,10 +45,32 @@ class MediaList extends React.Component {
       return diff;
     });
 
+    if (this.state.type) {
+      media = media.filter(m => m.mimetype.startsWith(`${this.state.type}/`));
+    }
+
     return (
       <div id="content" className="vertical">
         <div className="horizontal" style={{ justifyContent: "space-between" }}>
           <h2>{this.props.title}</h2>
+          <div id="mediaSelector">
+            <If condition={this.state.type == "image"}>
+              <Then>
+                <i onClick={() => this.selectType(null)} className="material-icons selected" title="Show all">photo</i>
+              </Then>
+              <Else>
+                <i onClick={() => this.selectType("image")} className="material-icons" title="Show photos">photo</i>
+              </Else>
+            </If>
+            <If condition={this.state.type == "video"}>
+              <Then>
+                <i onClick={() => this.selectType(null)} className="material-icons selected" title="Show all">movie</i>
+              </Then>
+              <Else>
+                <i onClick={() => this.selectType("video")} className="material-icons" title="Show videos">movie</i>
+              </Else>
+            </If>
+          </div>
           <label>Sort:
             <select style={{ marginLeft: "10px" }} onChange={this.onChangeSort} value={this.state.sort}>
               <option value="descending">Newest to oldest</option>
