e06172d33c298bbb43581526f867b1744ec9afda
Dave Townsend <dtownsend@oxymoronical.com>
2019-09-26 16:37:18 -0700

    Only authorize with backblaze when necessary.

--- api/backblaze.py
+++ api/backblaze.py
@@ -5,12 +5,15 @@ from base.utils import config
 B2_AUTHORIZE_URL = 'https://api.backblazeb2.com/b2api/v1/b2_authorize_account'
 
 class Backblaze(object):
+    auth_token = None
+    api_url = None
+    download_url = None
+
     def __init__(self, key_id, key, bucket_id, bucket_name):
         self.key_id = key_id
         self.key = key
         self.bucket_id = bucket_id
         self.bucket_name = bucket_name
-        self.authorize()
 
     def get_endpoint(self, version, method):
         return '%s/b2api/v%s/%s' % (self.api_url, version, method)
@@ -25,7 +28,14 @@ class Backblaze(object):
         self.api_url = data['apiUrl']
         self.download_url = data['downloadUrl']
 
+    def ensure_authorized(self):
+        if self.auth_token != None:
+            return
+        self.authorize()
+
     def upload(self, path, sha1, file, content_type='application/octet-stream'):
+        self.ensure_authorized()
+
         for i in range(5):
             url = self.get_endpoint(1, 'b2_get_upload_url')
             headers = {
@@ -64,6 +74,8 @@ class Backblaze(object):
         raise Exception('Failed to upload file after five attempts.')
 
     def delete(self, path, id):
+        self.ensure_authorized()
+
         url = self.get_endpoint(1, 'b2_delete_file_version')
         headers = {
             'Authorization': self.auth_token,
@@ -84,6 +96,8 @@ class Backblaze(object):
             raise Exception('Failed to delete file: %s %s' % (response.status_code, data['code']))
 
     def get_download_url(self, path, duration = 3600):
+        self.ensure_authorized()
+
         url = self.get_endpoint(1, 'b2_get_download_authorization')
         headers = {
             'Authorization': self.auth_token,
