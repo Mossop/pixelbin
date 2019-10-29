8e8a58cb2b9485b2d4fb7c96e1aaa0a6766baae9
Dave Townsend <dtownsend@oxymoronical.com>
2019-10-28 22:58:58 -0700

    Fix catalog page on login.

--- app/js/store/reducer.ts
+++ app/js/store/reducer.ts
@@ -126,7 +126,7 @@ function authReducer(state: Draft<StoreState>, action: ActionType): void {
       if (action.payload.user) {
         let catalogs = catalogNameSorted(action.payload.user.catalogs);
         if (catalogs.length) {
-          state.historyState = navigate(`/album/${catalogs[0].root.id}`);
+          state.historyState = navigate(`/catalog/${catalogs[0].id}`);
         } else {
           state.historyState = navigate("/user");
           if (!action.payload.user.hadCatalog) {
