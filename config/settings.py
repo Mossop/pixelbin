afb08014e286ea8fe68835ffd995911c70c7cb76
Dave Townsend <dtownsend@oxymoronical.com>
2020-04-06 22:00:34 -0700

    Use new logging configuration.

--- config/settings.py
+++ config/settings.py
@@ -8,3 +8,14 @@ INSTALLED_APPS = [
 
 if importlib.util.find_spec("django_extensions") is not None:
     INSTALLED_APPS.insert(0, 'django_extensions')
+
+LOGGING = {
+    'loggers': {
+        'app': {
+            'level': 'DEBUG',
+        },
+        'api': {
+            'level': 'DEBUG',
+        },
+    },
+}
