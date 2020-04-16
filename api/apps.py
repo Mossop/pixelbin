2572de6db3ca6819338b6c0ff5bebbdc65305651
Dave Townsend <dtownsend@oxymoronical.com>
2020-04-15 23:00:15 -0700

    Ignore erroneous errors.

--- api/apps.py
+++ api/apps.py
@@ -9,9 +9,11 @@ def startup(sender, **kwargs):
     from .tasks import PROCESS_VERSION, process_new_file, process_metadata
 
     for media in Media.objects.exclude(process_version=PROCESS_VERSION).filter(new_file=False):
+        # pylint: disable=no-value-for-parameter
         process_metadata(media.id)
 
     for media in Media.objects.filter(new_file=True):
+        # pylint: disable=no-value-for-parameter
         process_new_file(media.id)
 
 class ApiConfig(AppConfig):
