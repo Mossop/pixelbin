979b708af547c4503c5d20e1815064b31ace1abb
Dave Townsend <dtownsend@oxymoronical.com>
2020-04-21 10:17:35 -0700

    Add catalog API test.

---
+++ app/tests/api/catalog.ts
@@ -0,0 +1,57 @@
+import { createCatalog } from "../../js/api/catalog";
+import fetch from "../../js/environment/fetch";
+import { expect, mockedFunction } from "../helpers";
+import {
+  mockResponse,
+  MockResponse,
+  callInfo,
+  CatalogDataResponse,
+} from "../helpers/api";
+
+jest.mock("../../js/environment/fetch");
+
+const mockedFetch = mockedFunction(fetch);
+
+beforeEach((): void => {
+  mockedFetch.mockClear();
+});
+
+document.cookie = "csrftoken=csrf-foobar";
+
+test("Create catalog", async (): Promise<void> => {
+  mockResponse(mockedFetch, new MockResponse<CatalogDataResponse>(200, {
+    id: "testcatalog",
+    name: "Test catalog",
+    people: [],
+    tags: [],
+    albums: [],
+  }));
+
+  let result = await createCatalog("Test catalog", {
+    type: "server",
+  });
+
+  expect(result).toEqual({
+    id: "testcatalog",
+    name: "Test catalog",
+    people: new Map(),
+    tags: new Map(),
+    albums: new Map(),
+  });
+
+  let info = callInfo(mockedFetch);
+  expect(info).toEqual({
+    method: "PUT",
+    path: "http://pixelbin/api/catalog/create",
+    headers: {
+      "X-CSRFToken": "csrf-foobar",
+      "Content-Type": "application/json",
+    },
+    body: {
+      name: "Test catalog",
+      storage: {
+        type: "server",
+      },
+    },
+  });
+});
