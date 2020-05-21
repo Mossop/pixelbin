feb2e69ed05ec8f3e1569d0c1c789901034e9ba0
Dave Townsend <dtownsend@oxymoronical.com>
2020-05-21 09:05:43 -0700

    Switch to calling webpack directly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 /* eslint-env node */
-const { src, dest, series, parallel, watch } = require("gulp");
+const { src, dest, parallel, watch } = require("gulp");
 const gulpSass = require("gulp-sass");
 const mergeStreams = require("merge-stream");
-const named = require("vinyl-named");
-const gulpWebpack = require("webpack-stream");
+const webpack = require("webpack");
 
 const { config, path } = require("./base/config");
 const { eslintCheck } = require("./ci/eslint");
 const { pylintCheck } = require("./ci/pylint");
 const { typeScriptCheck } = require("./ci/typescript");
 const { logLints } = require("./ci/utils");
+const webpackConfig = require("./webpack.config");
 
 /**
- * @typedef { import("webpack").Configuration } Configuration
- * @typedef { import("webpack").RuleSetQuery } RuleSetQuery
+ * @typedef { import("webpack").Stats } Stats
  */
 
 const IGNORES = [
@@ -37,79 +36,6 @@ function allScripts() {
     "**/*.tsx",
     ...IGNORES,
   ];
-}
-
-/**
- * @return {RuleSetQuery}
- */
-function babelOptions() {
-  return {
-    passPerPreset: true,
-    plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-optional-chaining",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-    ],
-    presets: [
-      [
-        "@babel/preset-typescript", {
-          isTSX: true,
-          allExtensions: true,
-        },
-      ],
-      ["@babel/preset-react"],
-      [
-        "@babel/preset-env", {
-          targets: "defaults",
-          useBuiltIns: "usage",
-          corejs: 3,
-        },
-      ],
-    ],
-  };
-}
-
-/**
- * @return {Configuration}
- */
-function buildJsConfig() {
-  return {
-    mode: config.general.debug ? "development" : "production",
-    resolve: {
-      extensions: [".js", ".ts", ".tsx"],
-    },
-    output: {
-      publicPath: `${config.url.static}app/js/`,
-      filename: "app.js",
-      chunkFilename: "[name].js",
-    },
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.[jt]sx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: babelOptions(),
-          },
-        },
-      ],
-    },
-    externals: {
-      "react": "React",
-      "react-dom": "ReactDOM",
-    },
-  };
-}
-
-/**
- * @return {Configuration}
- */
-function watchJsConfig() {
-  let config = buildJsConfig();
-  config.watch = true;
-  return config;
 }
 
 /**
@@ -152,24 +78,27 @@ exports.watchLint = function() {
 };
 
 /**
- * @return {NodeJS.ReadWriteStream}
+ * @return {Promise<void>}
  */
-exports.watchBuildJs = function() {
-  return src([path("app", "js", "bootstrap.tsx")])
-    .pipe(named())
-    .pipe(gulpWebpack(watchJsConfig()))
-    .pipe(dest(path(config.path.build, "app", "js")));
-};
+async function buildJs() {
+  let compiler = webpack(webpackConfig);
 
-/**
- * @return {NodeJS.ReadWriteStream}
- */
-exports.buildJs = function() {
-  return src([path("app", "js", "bootstrap.tsx")])
-    .pipe(named())
-    .pipe(gulpWebpack(buildJsConfig()))
-    .pipe(dest(path(config.path.build, "app", "js")));
-};
+  /** @type {Stats} */
+  let stats = await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stats);
+      }
+    });
+  });
+
+  console.log(stats.toString({
+    colors: true,
+  }));
+}
 
 /**
  * @return {NodeJS.ReadWriteStream}
@@ -187,10 +116,6 @@ exports.watchBuildCss = function() {
   watch(["**/*.scss", ...IGNORES], exports.buildCss);
 };
 
-exports.build = parallel(exports.buildJs, exports.buildCss);
-exports.watchBuild = series(
-  exports.buildCss,
-  parallel(exports.watchBuildJs, exports.watchBuildCss),
-);
+exports.build = parallel(buildJs, exports.buildCss);
 
 exports.default = exports.build;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const { config } = require("./base/config");
+
+/**
+ * @typedef { import("webpack").Configuration } Configuration
+ */
+
+/** @type {Configuration} */
+module.exports = {
+  mode: config.general.debug ? "development" : "production",
+  entry: path.join(__dirname, "app", "js", "bootstrap.tsx"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  output: {
+    path: path.join(__dirname, "build", "app", "js"),
+    publicPath: `${config.url.static}app/js/`,
+    filename: "app.js",
+    chunkFilename: "[name].js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  },
+};
