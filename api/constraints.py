0b9bb9d0bdd49f5cd14273d3a874a5333393aff1
Dave Townsend <dtownsend@oxymoronical.com>
2020-06-04 07:39:15 -0700

    Add types to constraints.

--- api/constraints.py
+++ api/constraints.py
@@ -1,11 +1,15 @@
+from typing import Sequence, Optional, Type, Any, Tuple, Mapping, List
+
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 from django.db.backends.ddl_references import Statement, Table, Columns
+from django.db.models import Model
 from django.db.models.constraints import BaseConstraint
 from django.db.models.sql.query import Query
-from django.db.models.expressions import Func, F
+from django.db.models.expressions import BaseExpression, Func, F
 
 USE_INDEX = ['sqlite', 'postgresql']
 
-def resolve(expr, query):
+def resolve(expr: BaseExpression, query: Query) -> BaseExpression:
     if isinstance(expr, Func):
         e = expr.copy()
         e.is_summary = False
@@ -19,12 +23,15 @@ def resolve(expr, query):
     return expr.resolve_expression(query=query, allow_joins=False)
 
 class UniqueWithExpressionsConstraint(BaseConstraint):
-    def __init__(self, *, name, fields, expressions=None):
+    fields: Sequence[str]
+    expressions: Sequence[BaseExpression]
+
+    def __init__(self, *, name: str, fields: Sequence[str], expressions: Sequence[BaseExpression]):
         self.fields = fields
         self.expressions = expressions
         super().__init__(name=name)
 
-    def _get_terms(self, model, schema_editor):
+    def _get_terms(self, model: Type[Model], schema_editor: BaseDatabaseSchemaEditor) -> List[str]:
         query = Query(model)
         compiler = query.get_compiler(connection=schema_editor.connection)
 
@@ -37,7 +44,10 @@ class UniqueWithExpressionsConstraint(BaseConstraint):
 
         return terms
 
-    def constraint_sql(self, model, schema_editor):
+    def constraint_sql( # type: ignore
+            self, model: Type[Model],
+            schema_editor: BaseDatabaseSchemaEditor
+    ) -> Optional[str]:
         if schema_editor.connection.vendor in USE_INDEX:
             sql = self.create_sql(model, schema_editor)
             if sql:
@@ -45,46 +55,54 @@ class UniqueWithExpressionsConstraint(BaseConstraint):
             return None
 
         terms = self._get_terms(model, schema_editor)
-        constraint = schema_editor.sql_unique_constraint % {
+        constraint = schema_editor.sql_unique_constraint % { # type: ignore
             'columns': ', '.join(terms),
         }
-        return schema_editor.sql_constraint % {
+        return schema_editor.sql_constraint % { # type: ignore
             'name': schema_editor.quote_name(self.name),
             'constraint': constraint,
         }
 
-    def create_sql(self, model, schema_editor):
+    def create_sql( # type: ignore
+            self,
+            model: Type[Model],
+            schema_editor: BaseDatabaseSchemaEditor
+    ) -> str:
         terms = self._get_terms(model, schema_editor)
         table = Table(model._meta.db_table, schema_editor.quote_name)
         name = schema_editor.quote_name(self.name)
-        columns = Columns(table, terms, str)
+        columns = Columns(str(table), terms, str)
 
         if schema_editor.connection.vendor in USE_INDEX:
-            template = schema_editor.sql_create_unique_index
+            template = schema_editor.sql_create_unique_index # type: ignore
         else:
             template = schema_editor.sql_create_unique
 
-        return Statement(
+        return str(Statement(
             template,
             table=table,
             name=name,
             columns=columns,
             condition='',
-        )
+        ))
 
-    def remove_sql(self, model, schema_editor):
+    def remove_sql( # type: ignore
+            self,
+            model: Type[Model],
+            schema_editor: BaseDatabaseSchemaEditor
+    ) -> str:
         # pylint: disable=protected-access
         if schema_editor.connection.vendor in USE_INDEX:
-            return schema_editor._delete_index_sql(model, self.name)
+            return schema_editor._delete_index_sql(model, self.name) # type: ignore
 
-        return schema_editor._delete_unique_sql(model, self.name)
+        return schema_editor._delete_unique_sql(model, self.name) # type: ignore
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<%s: fields=%r expressions=%r name=%r>' % (
             self.__class__.__qualname__, self.fields, self.expressions, self.name,
         )
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         if isinstance(other, self.__class__):
             return (
                 self.name == other.name and
@@ -93,7 +111,7 @@ class UniqueWithExpressionsConstraint(BaseConstraint):
             )
         return super().__eq__(other)
 
-    def deconstruct(self):
+    def deconstruct(self) -> Tuple[str, Sequence[Any], Mapping[str, Any]]:
         path, args, kwargs = super().deconstruct()
         kwargs['fields'] = self.fields
         kwargs['expressions'] = self.expressions
