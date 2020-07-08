3d6b233c3eeac482fd3b5ed607704ecfe7d69167
Dave Townsend <dtownsend@oxymoronical.com>
2020-07-08 16:08:49 -0700

    Module upgrades.

--- src/client/js/l10n/index.tsx
+++ src/client/js/l10n/index.tsx
@@ -1,11 +1,11 @@
-import { FluentArgument } from "@fluent/bundle";
+import { FluentVariable } from "@fluent/bundle";
 import { LocalizedProps } from "@fluent/react";
 
 export type L10nInfo = string | LocalizedProps;
 export type L10nProps = { l10n: L10nInfo };
 export type OptionalL10nProps = Partial<L10nProps>;
 
-export function l10nInfo(id: string, vars?: Record<string, FluentArgument>): L10nInfo {
+export function l10nInfo(id: string, vars?: Record<string, FluentVariable>): L10nInfo {
   if (!vars || [...Object.entries(vars)].length == 0) {
     return id;
   }
