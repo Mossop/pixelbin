3aa728ee5e70d21494607219aaf7076b8a0ece95
Dave Townsend <dtownsend@oxymoronical.com>
2020-08-25 18:26:08 -0700

    Remove old python API.

--- api/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .catalog import Catalog, Album, Tag, Person
-from .user import User, Access
-from .media import Media, MediaInfo, MediaTag, MediaAlbum, MediaPerson
-from ..storage.models import Server, Backblaze
--- config/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-APP_URL_PATTERNS = [
-    path('api/', include('api.urls')),
-    path('', include('app.urls')),
-]
