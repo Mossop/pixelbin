9dd46a9fc6db217c29866bf7f67546520ef3560d
Dave Townsend <dtownsend@oxymoronical.com>
2020-09-02 20:58:44 -0700

    Remove unused code.

--- src/client/js/components/Throbber.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode, PureComponent } from "react";
-
-export default class Throbber extends PureComponent {
-  public render(): ReactNode {
-    return <div className="throbber"/>;
-  }
-}
