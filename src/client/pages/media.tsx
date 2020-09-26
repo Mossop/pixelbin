dc8c81448bf8133b9a14be52039ef3c6669dd8a6
Dave Townsend <dtownsend@oxymoronical.com>
2020-09-25 23:47:49 -0700

    Move overlay handling up a level.

--- src/client/pages/media.tsx
+++ src/client/pages/media.tsx
@@ -1,4 +1,3 @@
-import Box from "@material-ui/core/Box";
 import Fade from "@material-ui/core/Fade";
 import IconButton from "@material-ui/core/IconButton";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
@@ -12,6 +11,7 @@ import { isProcessed, MediaState, ProcessedMediaState } from "../api/types";
 import FixedAspect from "../components/FixedAspect";
 import Loading from "../components/Loading";
 import Page from "../components/Page";
+import { document } from "../environment";
 import { useSelector } from "../store";
 import { useActions } from "../store/actions";
 import { MediaLookup, MediaLookupType, StoreState } from "../store/types";
@@ -35,15 +35,15 @@ const useMainStyles = makeStyles(() =>
       width: "100%",
       background: "black",
     },
-  }));
-
-const useOverlayStyles = makeStyles((theme: Theme) =>
-  createStyles({
     overlay: {
       position: "absolute",
       height: "100%",
       width: "100%",
     },
+  }));
+
+const useOverlayStyles = makeStyles((theme: Theme) =>
+  createStyles({
     overlayContent: {
       height: "100%",
       width: "100%",
@@ -65,19 +65,25 @@ const useOverlayStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(2),
     },
     navButton: {
-      fontSize: "300%",
-      color: overlayIcon,
-      background: overlayBackground,
+      "fontSize": "4rem",
+      "color": overlayIcon,
+      "background": overlayBackground,
+      "& .MuiSvgIcon-root": {
+        fontSize: "inherit",
+      },
     },
     overlayButton: {
-      fontSize: "150%",
-      color: overlayIcon,
+      "fontSize": "2rem",
+      "color": overlayIcon,
+      "& .MuiSvgIcon-root": {
+        fontSize: "inherit",
+      },
     },
   }));
 
 const useMediaStyles = makeStyles(() =>
   createStyles({
-    photo: (media: ProcessedMediaState) => ({
+    media: (media: ProcessedMediaState) => ({
       objectPosition: "center center",
       objectFit: "scale-down",
       height: "100%",
@@ -100,23 +106,28 @@ interface MediaOverlayProps {
   media: MediaState;
   onNext?: (() => void) | null;
   onPrevious?: (() => void) | null;
+  onGoFullscreen?: () => void;
+  onExitFullscreen?: () => void;
 }
 
 function Photo(props: ProcessedMediaProps): ReactResult {
   const classes = useMediaStyles(props.media);
-  console.log("Rendering photo", props.media.originalUrl);
 
-  return <img src={props.media.originalUrl} className={classes.photo}/>;
+  return <img
+    key={props.media.id}
+    src={props.media.originalUrl}
+    className={classes.media}
+  />;
 }
 
 function Video(props: ProcessedMediaProps): ReactResult {
   const classes = useMediaStyles(props.media);
-  console.log("Rendering video", props.media.originalUrl);
 
   return <video
+    key={props.media.id}
     poster={props.media.posterUrl ?? undefined}
     controls={true}
-    className={classes.photo}
+    className={classes.media}
   >
     <source src={props.media.originalUrl} type={props.media.mimetype}/>
   </video>;
@@ -124,84 +135,53 @@ function Video(props: ProcessedMediaProps): ReactResult {
 
 function MediaOverlay(props: MediaOverlayProps): ReactResult {
   const classes = useOverlayStyles();
-  const areaRef = useRef<HTMLDivElement>(null);
-  const [displayOverlays, setDisplayOverlays] = useState(false);
   const fullscreen = useFullscreen();
 
-  const goFullscreen = useCallback(() => {
-    if (!areaRef.current) {
-      return;
-    }
-
-    void areaRef.current.parentElement?.requestFullscreen();
-  }, [areaRef]);
-
-  const exitFullscreen = useCallback(() => {
-    if (!areaRef.current) {
-      return;
-    }
-
-    void areaRef.current.ownerDocument.exitFullscreen();
-  }, [areaRef]);
-
-  const hideOverlays = useCallback(() => {
-    console.log("hideOverlays");
-    setDisplayOverlays(false);
-  }, []);
-
-  const delayed = useMemo(() => new Delayed(1500, hideOverlays), [hideOverlays]);
-
-  const showOverlays = useCallback(() => {
-    console.log("showOverlays");
-    setDisplayOverlays(true);
-    delayed.trigger();
-  }, [delayed]);
-
-  return <div
-    ref={areaRef}
-    className={classes.overlay}
-    onMouseOver={showOverlays}
-    onMouseMove={showOverlays}
-  >
-    <Fade in={displayOverlays} timeout={500}>
-      <div className={classes.overlayContent}>
-        <div className={classes.overlayTop}>
-          {
-            fullscreen
-              ? <IconButton onClick={exitFullscreen}>
-                <FullscreenExitIcon className={classes.overlayButton}/>
-              </IconButton>
-              : <IconButton onClick={goFullscreen}>
-                <FullscreenIcon className={classes.overlayButton}/>
-              </IconButton>
-          }
-        </div>
-        <div className={classes.overlayMiddle}>
-          <div>
-            {
-              props.onPrevious && <IconButton onClick={props.onPrevious}>
-                <NavigateBeforeIcon className={classes.navButton}/>
-              </IconButton>
-            }
-          </div>
-          <div>
-            {
-              props.onNext && <IconButton onClick={props.onNext}>
-                <NavigateNextIcon className={classes.navButton}/>
-              </IconButton>
-            }
-          </div>
-        </div>
+  return <div className={classes.overlayContent}>
+    <div className={classes.overlayTop}>
+      {
+        fullscreen
+          ? <IconButton onClick={props.onExitFullscreen} className={classes.overlayButton}>
+            <FullscreenExitIcon/>
+          </IconButton>
+          : <IconButton onClick={props.onGoFullscreen} className={classes.overlayButton}>
+            <FullscreenIcon/>
+          </IconButton>
+      }
+    </div>
+    <div className={classes.overlayMiddle}>
+      <div>
+        {
+          props.onPrevious && <IconButton
+            onClick={props.onPrevious}
+            className={classes.navButton}
+          >
+            <NavigateBeforeIcon/>
+          </IconButton>
+        }
       </div>
-    </Fade>
+      <div>
+        {
+          props.onNext && <IconButton
+            onClick={props.onNext}
+            className={classes.navButton}
+          >
+            <NavigateNextIcon/>
+          </IconButton>
+        }
+      </div>
+    </div>
   </div>;
 }
 
 export default function MediaPage(props: MediaPageProps & AuthenticatedPageProps): ReactResult {
   const actions = useActions();
   const classes = useMainStyles();
+  const areaRef = useRef<HTMLDivElement>(null);
 
   const mediaList = useSelector((state: StoreState) => state.mediaList?.media);
+
+  const [displayOverlays, setDisplayOverlays] = useState(false);
 
   useEffect(
     () => {
@@ -250,6 +230,25 @@ export default function MediaPage(props: MediaPageProps & AuthenticatedPageProps
     };
   }, [mediaIndex, mediaList, actions, props.lookup]);
 
+  const hideOverlays = useCallback(() => {
+    setDisplayOverlays(false);
+  }, []);
+
+  const delayed = useMemo(() => new Delayed(1500, hideOverlays), [hideOverlays]);
+
+  const showOverlays = useCallback(() => {
+    setDisplayOverlays(true);
+    delayed.trigger();
+  }, [delayed]);
+
+  const goFullscreen = useCallback(() => {
+    void areaRef.current?.requestFullscreen();
+  }, [areaRef]);
+
+  const exitFullscreen = useCallback(() => {
+    void document.exitFullscreen();
+  }, []);
+
   if (!mediaList) {
     return <Page>
       <Loading flexGrow={1}/>
@@ -265,7 +264,12 @@ export default function MediaPage(props: MediaPageProps & AuthenticatedPageProps
   const media = mediaList[mediaIndex];
 
   return <Page sidebar="openable">
-    <Box className={classes.content}>
+    <div
+      className={classes.content}
+      ref={areaRef}
+      onMouseOver={showOverlays}
+      onMouseMove={showOverlays}
+    >
       {
         isProcessed(media)
           ? <FixedAspect
@@ -285,11 +289,17 @@ export default function MediaPage(props: MediaPageProps & AuthenticatedPageProps
           </FixedAspect>
           : <Loading className={classes.mediaArea}/>
       }
-      <MediaOverlay
-        media={media}
-        onPrevious={onPrevious}
-        onNext={onNext}
-      />
-    </Box>
+      <Fade in={displayOverlays} timeout={500}>
+        <div className={classes.overlay}>
+          <MediaOverlay
+            media={media}
+            onPrevious={onPrevious}
+            onNext={onNext}
+            onGoFullscreen={goFullscreen}
+            onExitFullscreen={exitFullscreen}
+          />
+        </div>
+      </Fade>
+    </div>
   </Page>;
 }
