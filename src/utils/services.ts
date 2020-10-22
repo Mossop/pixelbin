c5ecf4fa0ccdcb04bba808c1a50217db5e445f35
Dave Townsend <dtownsend@oxymoronical.com>
2020-10-22 10:41:46 -0700

    Undo the services changes.

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,65 +1,27 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Deferred } from "./defer";
+import { entries } from "./utility";
 
-type ServiceType<S> =
-  S extends Deferred<infer T>
-    ? T
-    : S extends () => Promise<infer T>
-      ? T
-      : S extends () => infer T
-        ? T
-        : S extends Promise<infer T>
-          ? T
-          : S;
+type ServiceType<SM, S extends keyof SM> = SM[S] extends Deferred<infer T> ? T : never;
 
 type Services<SM> = {
-  [S in keyof SM]: Promise<ServiceType<SM[S]>>;
+  [S in keyof SM]: Promise<ServiceType<SM, S>>;
 };
 
 export function buildServices<SM>(map: SM): Services<SM> {
-  const resolveService = <S extends keyof SM>(key: S): Promise<ServiceType<SM[S]>> => {
-    if (typeof map[key] == "function") {
-      try {
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return Promise.resolve(map[key]());
-      } catch (e) {
-        return Promise.reject(e);
-      }
-    }
-
-    if (typeof map[key] == "object" && "promise" in map[key]) {
-      // @ts-ignore
+  return Object.fromEntries(
+    entries(map).map(
+      <S extends keyof SM>(
+        [key, value]: [S, SM[S]],
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return map[key].promise;
-    }
-
-    // @ts-ignore
-    return Promise.resolve(map[key]);
-  };
-
-  let registry: Partial<Services<SM>> = {};
-  return new Proxy<Services<SM>>(registry as Services<SM>, {
-    get<S extends keyof SM>(target: Services<SM>, property: S): Promise<ServiceType<SM[S]>> {
-      if (!(property in registry)) {
-        registry[property] = resolveService(property);
-      }
-
-      // @ts-ignore
-      return registry[property];
-    },
-  });
+      ): [S, Promise<ServiceType<SM, S>>] => [key, value["promise"]],
+    ),
+  ) as unknown as Services<SM>;
 }
 
-type DeferredServices<SM> = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [K in keyof SM]: SM[K] extends Deferred<any> ? K : never;
-}[keyof SM];
-
-type ServiceResolution<SM, S extends keyof SM> = ServiceType<SM[S]> | Promise<ServiceType<SM[S]>>;
+type ServiceResolution<SM, S extends keyof SM> = ServiceType<SM, S> | Promise<ServiceType<SM, S>>;
 export function serviceProvider<SM>(
   map: SM,
-): <S extends DeferredServices<SM>>(service: S, value: ServiceResolution<SM, S>) => void {
+): <S extends keyof SM>(service: S, value: ServiceResolution<SM, S>) => void {
   return <S extends keyof SM>(service: S, value: ServiceResolution<SM, S>): void => {
     // @ts-ignore: The properties of SM cannot be defined.
     map[service].resolve(value);
