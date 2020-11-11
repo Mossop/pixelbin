bd76f097da46a116eeb2f04b0f90aa675ad82f51
Dave Townsend <dtownsend@oxymoronical.com>
2020-11-11 12:49:14 -0800

    Make sure to close services when failing to seed.

--- src/server/main/seed.ts
+++ src/server/main/seed.ts
@@ -11,37 +11,39 @@ import Services from "./services";
 const logger = getLogger("pixelbin");
 
 export default async function seed(options: SeedOptions): Promise<void> {
-  let file = path.resolve(options.file);
-  logger.info(`Seeding database from ${options.file}`);
-
-  let fileContent: string;
   try {
-    fileContent = await fs.readFile(file, {
-      encoding: "utf8",
-    });
-  } catch (e) {
-    logger.error(e, `Failed to read seed file ${file}`);
-    return;
+    let file = path.resolve(options.file);
+    logger.info(`Seeding database from ${options.file}`);
+
+    let fileContent: string;
+    try {
+      fileContent = await fs.readFile(file, {
+        encoding: "utf8",
+      });
+    } catch (e) {
+      logger.error(e, `Failed to read seed file ${file}`);
+      return;
+    }
+
+    let content: unknown;
+    try {
+      content = JSON.parse(fileContent);
+    } catch (e) {
+      logger.error(e, `Failed to parse seed file ${file}`);
+      return;
+    }
+
+    let seed: Seed;
+    try {
+      seed = await SeedDecoder.decodePromise(content);
+    } catch (e) {
+      logger.error(e, `Failed to parse seed file ${file}`);
+      return;
+    }
+
+    let connection = await Services.database;
+    await connection.seed(seed);
+  } finally {
+    quit();
   }
-
-  let content: unknown;
-  try {
-    content = JSON.parse(fileContent);
-  } catch (e) {
-    logger.error(e, `Failed to parse seed file ${file}`);
-    return;
-  }
-
-  let seed: Seed;
-  try {
-    seed = await SeedDecoder.decodePromise(content);
-  } catch (e) {
-    logger.error(e, `Failed to parse seed file ${file}`);
-    return;
-  }
-
-  let connection = await Services.database;
-  await connection.seed(seed);
-
-  quit();
 }
