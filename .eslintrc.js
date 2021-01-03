2445e5587eb6064833edf234a130fe9062f41ddf
Dave Townsend <dtownsend@oxymoronical.com>
2021-01-03 14:15:09 -0800

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,6 @@ module.exports = {
     project: ["./tsconfig.json"],
   },
 
-  settings: {
-    "import/ignore": [
-      "typescript",
-    ],
-  },
-
   env: {
     node: true,
     es6: true,
@@ -59,132 +53,11 @@ module.exports = {
         from: "./src/server",
       }],
     }],
-
-    "no-multiple-empty-lines": ["warn", {
-      max: 1,
-      maxBOF: 0,
-      maxEOF: 0,
-    }],
-
-    "@typescript-eslint/naming-convention": ["warn",
-      {
-        selector: "default",
-        format: ["camelCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "variable",
-        modifiers: ["const", "global"],
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "variable",
-        modifiers: ["const", "global", "exported"],
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: ["variable", "parameter"],
-        modifiers: ["destructured"],
-        format: null,
-      },
-      {
-        selector: ["function"],
-        format: ["camelCase", "PascalCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: ["variable", "function"],
-        modifiers: ["exported"],
-        format: ["camelCase", "PascalCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "variableLike",
-        format: ["camelCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "parameter",
-        format: ["camelCase"],
-        leadingUnderscore: "allow",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "objectLiteralProperty",
-        format: null,
-        types: ["boolean"],
-        filter: {
-          regex: "^__esModule$",
-          match: true,
-        },
-        custom: {
-          regex: "^__esModule$",
-          match: true,
-        },
-      },
-      {
-        selector: "memberLike",
-        modifiers: ["private"],
-        format: ["camelCase"],
-        leadingUnderscore: "allow",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "memberLike",
-        modifiers: ["protected"],
-        format: ["camelCase"],
-        leadingUnderscore: "allow",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "memberLike",
-        format: ["camelCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "enumMember",
-        format: ["PascalCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      },
-      {
-        selector: "typeLike",
-        format: ["PascalCase"],
-        leadingUnderscore: "forbid",
-        trailingUnderscore: "forbid",
-      }],
   },
 
   ignorePatterns: ["dist/**/*"],
 
   overrides: [{
-    files: [
-      "**/*.test.js",
-      "**/*.test.jsx",
-      "**/*.test.ts",
-      "**/*.test.tsx",
-      "**/__mocks__/*.js",
-      "**/__mocks__/*.ts",
-      "**/test-helpers*",
-    ],
-
-    env: {
-      jest: true,
-    },
-
-    rules: {
-      "@typescript-eslint/unbound-method": "off",
-    },
-  }, {
     files: ["*.ts", "*.tsx"],
   }],
 };
