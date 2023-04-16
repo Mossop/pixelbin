17c65b75a7311a0f6ca3f733be788f7c694c8a32
Dave Townsend <dtownsend@oxymoronical.com>
2023-04-16 07:01:59 +0100

    Re-up workers

--- src/server/main/tasks.ts
+++ src/server/main/tasks.ts
@@ -41,8 +41,8 @@ export class TaskManager extends Service {
     this.config = {
       ...config,
       minWorkers: config.minWorkers ?? 0,
-      maxWorkers: config.maxTasksPerWorker ?? 1,
-      maxTasksPerWorker: config.maxTasksPerWorker ?? 1,
+      maxWorkers: config.maxTasksPerWorker ?? 2,
+      maxTasksPerWorker: config.maxTasksPerWorker ?? 2,
     };
 
     let module = path.resolve(path.join(path.dirname(__dirname), "task-worker"));
