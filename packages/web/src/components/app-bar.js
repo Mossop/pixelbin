2abc7242d4d775308d92f8a3e8994226195637e7
Dave Townsend <dtownsend@oxymoronical.com>
2023-06-04 12:19:51 +0100

    Add basic header

--- packages/web/src/components/app-bar.js
+++ packages/web/src/components/app-bar.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("app-bar")
@@ -6,7 +6,29 @@ export class AppBar extends LitElement {
   @property()
   title = "";
 
+  static styles = css`
+    header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      color: var(--accent-color);
+      background: var(--accent-background);
+      padding: calc(2 * var(--padding));
+    }
+
+    h1 {
+      font-size: 1.5rem;
+      font-family: "Comfortaa", cursive;
+      font-weight: bold;
+      margin: 0;
+    }
+  `;
+
   render() {
-    return html`<header>${this.title}</header>`;
+    return html`<header>
+      <h1>${this.title}</h1>
+      <div><slot></slot></div>
+    </header>`;
   }
 }
