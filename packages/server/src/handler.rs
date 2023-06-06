c7e81179cf616e76aa56355f98922b14bd7c02a9
Dave Townsend <dtownsend@oxymoronical.com>
2023-06-06 05:12:10 +0100

    Stub out some API methods

--- packages/server/src/handler.rs
+++ packages/server/src/handler.rs
@@ -1,6 +1,8 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{body::BoxBody, get, http::StatusCode, post, web, HttpResponse, Responder};
 use mime_guess::from_path;
 use rust_embed::RustEmbed;
+use serde::{Deserialize, Serialize};
+use serde_with::{formats::CommaSeparator, serde_as, StringWithSeparator};
 
 use crate::util::HttpResult;
 use crate::AppState;
@@ -26,4 +28,125 @@ async fn static_files(path: web::Path<String>) -> impl Responder {
             .body(content.data.into_owned()),
         None => HttpResponse::NotFound().body("404 Not Found"),
     }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+enum ApiErrorCode {
+    UnknownException,
+    BadMethod,
+    NotLoggedIn,
+    LoginFailed,
+    InvalidData,
+    NotFound,
+    TemporaryFailure,
+    InvalidHost,
+}
+
+impl ApiErrorCode {
+    fn into<T>(self) -> ApiResult<T> {
+        ApiResult::Err(ApiError { code: self })
+    }
+}
+
+#[derive(Serialize)]
+struct ApiError {
+    code: ApiErrorCode,
+}
+
+impl Into<HttpResponse> for ApiError {
+    fn into(self) -> HttpResponse {
+        let code = match self.code {
+            ApiErrorCode::UnknownException => 500,
+            ApiErrorCode::BadMethod => 405,
+            ApiErrorCode::NotLoggedIn => 401,
+            ApiErrorCode::LoginFailed => 401,
+            ApiErrorCode::InvalidData => 400,
+            ApiErrorCode::NotFound => 404,
+            ApiErrorCode::TemporaryFailure => 503,
+            ApiErrorCode::InvalidHost => 403,
+        };
+
+        HttpResponse::build(StatusCode::from_u16(code).unwrap())
+            .content_type("application/json")
+            .body(serde_json::to_string_pretty(&self).unwrap())
+    }
+}
+
+enum ApiResult<T> {
+    Ok(T),
+    Err(ApiError),
+}
+
+impl<T> From<T> for ApiResult<T> {
+    fn from(result: T) -> Self {
+        Self::Ok(result)
+    }
+}
+
+impl<T: Serialize> Responder for ApiResult<T> {
+    type Body = BoxBody;
+
+    fn respond_to(self, req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        match self {
+            ApiResult::Ok(inner) => HttpResponse::Ok()
+                .content_type("application/json")
+                .body(serde_json::to_string_pretty(&inner).unwrap()),
+            ApiResult::Err(err) => err.into(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CatalogState {
+    id: String,
+    storage: String,
+    name: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct AlbumState {
+    id: String,
+    catalog: String,
+    parent: Option<String>,
+    name: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct UserState {
+    catalogs: Vec<CatalogState>,
+    albums: Vec<AlbumState>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ApiState {
+    user: Option<UserState>,
+    api_host: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct Credentials {
+    email: String,
+    password: String,
+}
+
+#[post("/api/login")]
+async fn api_login(credentials: web::Json<Credentials>) -> HttpResult<ApiResult<ApiState>> {
+    Ok(ApiErrorCode::NotLoggedIn.into())
+}
+
+#[serde_as]
+#[derive(Deserialize)]
+struct MediaList {
+    #[serde_as(as = "StringWithSeparator::<CommaSeparator, String>")]
+    id: Vec<String>,
+}
+
+#[get("/api/media/get")]
+async fn api_media_get(media_list: web::Query<MediaList>) -> HttpResult<HttpResponse> {
+    todo!();
 }
