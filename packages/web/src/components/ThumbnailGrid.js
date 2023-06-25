7ce41a48cbf079ca3219e98040bcad60e4b5a239
Dave Townsend <dtownsend@oxymoronical.com>
2023-06-25 08:40:35 +0100

    Simplify grid spacing

--- packages/web/src/components/ThumbnailGrid.js
+++ packages/web/src/components/ThumbnailGrid.js
@@ -1,33 +1,19 @@
-import { LitElement, html, css, unsafeCSS } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
 import { bootstrap } from "../modules/styles";
-
-let gridCss = [];
-for (let i = 2; i < 20; i++) {
-  gridCss.push(`
-    @container (width > calc(0.5rem + ${i} * calc(150px + 1.5rem))) {
-      div {
-        grid-template-columns: repeat(${i}, 1fr);
-      }
-    }
-  `);
-}
 
 @customElement("ui-thumbnailgrid")
 export class ThumbnailGrid extends LitElement {
   static styles = [
     bootstrap,
     css`
-      :host {
-        container-type: inline-size;
-        display: block;
-      }
-
       div {
-        grid-template-columns: repeat(1, 1fr);
+        grid-template-columns: repeat(
+          auto-fill,
+          minmax(calc(150px + 1rem), 1fr)
+        );
       }
     `,
-    unsafeCSS(gridCss.join("\n")),
   ];
 
   render() {
