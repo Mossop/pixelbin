4ea4bed64fca5c1b9d3c2f79aa1aa06047fe0651
Dave Townsend <dtownsend@oxymoronical.com>
2023-10-27 09:57:19 +0200

    Update to next.js 14

--- packages/webapp/src/app/layout.tsx
+++ packages/webapp/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Roboto } from "next/font/google";
 
 import AppBar from "@/components/AppBar";
@@ -12,13 +12,14 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: "PixelBin",
-  viewport: {
-    initialScale: 1,
-    minimumScale: 1,
-    maximumScale: 1,
-    width: "device-width",
-    height: "device-height",
-  },
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  width: "device-width",
+  height: "device-height",
 };
 
 export default function RootLayout({
