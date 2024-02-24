ee93830bf5616b93c63707121368eedb34adc3df
Dave Townsend <dtownsend@oxymoronical.com>
2024-02-24 12:33:27 +0000

    Re-introduce nav menu

--- packages/webapp/app/components/AppBar.tsx
+++ packages/webapp/app/components/AppBar.tsx
@@ -1,4 +1,5 @@
 import Avatar from "./Avatar";
+import NavMenu from "./NavMenu";
 import { useServerState } from "@/modules/client-util";
 
 export default function AppBar() {
@@ -8,6 +9,7 @@ export default function AppBar() {
   return (
     <header className="c-appbar" data-theme="banner">
       <div className="site">
+        {serverState && <NavMenu serverState={serverState} />}
         <h1>PixelBin</h1>
       </div>
       <div>
---
+++ packages/webapp/app/components/NavMenu.tsx
@@ -0,0 +1,43 @@
+import { useNavigation } from "@remix-run/react";
+import { useCallback, useEffect, useState } from "react";
+
+import { IconButton } from "./Icon";
+import { CatalogNav } from "./SidebarLayout";
+import SlidePanel from "./SlidePanel";
+import { State } from "@/modules/types";
+
+export default function NavMenu({ serverState }: { serverState: State }) {
+  let [showNavigator, setShowNavigator] = useState(false);
+  let openNavigator = useCallback(() => setShowNavigator(true), []);
+  let hideNavigator = useCallback(() => setShowNavigator(false), []);
+
+  let navigation = useNavigation();
+  useEffect(() => {
+    if (navigation.state != "idle") {
+      hideNavigator();
+    }
+  }, [navigation, hideNavigator]);
+
+  if (!serverState) {
+    return <div className="c-navmenu" />;
+  }
+
+  return (
+    <div className="c-navmenu">
+      <IconButton icon="menu" onClick={openNavigator} />
+      <SlidePanel
+        show={showNavigator}
+        position="left"
+        onClose={hideNavigator}
+        className="c-navigator"
+      >
+        <div className="viewport">
+          <div className="buttons">
+            <IconButton icon="close" onClick={hideNavigator} />
+          </div>
+          <CatalogNav serverState={serverState} />
+        </div>
+      </SlidePanel>
+    </div>
+  );
+}
